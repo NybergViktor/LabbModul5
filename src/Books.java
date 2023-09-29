0820697a0f789e89059d4c70f4e35857f6ed977d
Hamidreza Amininia <pascal.hmd@gmail.com>
2023-09-29 10:37:33 +0200

    first commit

--- src/Books.java
+++ src/Books.java
@@ -1,3 +1,43 @@
-public class Books{
+public class book {
+    private String namn, författare;
+    private double pris;
+
+    private boolean tillgängliga;
+
+    /*public book (){
+        namn = null;
+        författare = null;
+        pris = 0; }*/
+
+    public book(String namn, String författare, double pris) {
+        this.namn = namn ;
+        this.författare = författare;
+        this.pris = pris;
+        this.tillgängliga = true;
+
+    }
+
+    public String getFörfattare(){
+        return författare;
+    }
+    public String getNamn(){
+        return namn;
+
+    }
+    public double getpris(){
+        return pris;
+
+    }
+    public boolean isTillgängliga(){
+        return isTillgängliga();
+    }
+    public void låna(){
+        if (isTillgängliga()){
+
+
+        }
+
+    }
+
 
 }

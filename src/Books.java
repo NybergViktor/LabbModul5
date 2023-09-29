828c4a9e23b96c6403690415f2644f8104d3bf6d
Jonas Düring <polyfant@live.se>
2023-09-29 08:35:12 +0200

    testat lite med Books klassen

--- src/Books.java
+++ src/Books.java
@@ -1,2 +1,48 @@
 public class Books {
+
+        private String title;
+        private String author;
+        private boolean available;
+        private String description;
+
+
+    public Books(String title, String author, String description) {
+        this.title = title;
+        this.author = author;
+        this.available = true; // automatiskt tillgänglig vid skapande.
+        this.description = description;
+    }
+
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("labb modul 5");
+    Books bok1 = new Books("test", "test", "bla");
+        System.out.println(bok1.getAuthor() + bok1.getTitle() + bok1.getDescription());
+
     }
 }

0510b1a1dcb195df4ffc8acc3240af9374cb0008
Viktor Nyberg <viktor.nyberg00@gmail.com>
2023-09-29 11:10:45 +0200

    books och user

--- src/Books.java
+++ src/Books.java
@@ -1,2 +1,28 @@
+import java.util.ArrayList;
+
+record Item(String name, String Genre) {
+    public Item(String name) {
+        this(name, "Genre");
+    }
+}
+
 public class Books {
+
+    private int arraynr = 0;
+    Object bookList[] = new Object[10];
+
+    public ArrayList[] arrayBookList;
+
+    {
+        bookList[arraynr] = new Item("Hobbit", "Fantasy");
+        bookList[arraynr += 1] = new Item("HarryPotter 1", "Fantasy");
+        bookList[arraynr += 1] = new Item("HarryPotter 2", "Fantasy");
+        bookList[arraynr += 1] = new Item("HarryPotter 3", "Fantasy");
+        bookList[arraynr += 1] = new Item("HarryPotter 4", "Fantasy");
+        bookList[arraynr += 1] = new Item("HarryPotter 5", "Fantasy");
+        bookList[arraynr += 1] = new Item("HarryPotter 6", "Fantasy");
+        bookList[arraynr += 1] = new Item("HarryPotter 7 del 1", "Fantasy");
+        bookList[arraynr += 1] = new Item("HarryPotter 7 del 2", "Fantasy");
+        bookList[arraynr += 1] = new Item("Solsidan", "Comedy");
+    }
 }
--- src/User.java
+++ src/User.java
@@ -1,2 +1,49 @@
+import java.util.Scanner;
+
 public class User {
+    Scanner myScanner = new Scanner(System.in);
+
+    public void readListOfBooks(){
+
+        //här ska metod för att läsa upp alla böcker
+
+
+        System.out.println("Vill du låna en bok? ja/nej");
+        String alt = myScanner.nextLine();
+
+        while (true) {
+            if (alt == "ja") {
+                borrowBook();
+                break;
+            } else if (alt == "nej") {
+                break;
+            } else {
+                System.out.println("svara med ja eller nej");
+            }
+        }
+    }
+
+    public void borrowBook() {
+        System.out.println("");
+
+
+        int alt = myScanner.nextInt();
+        boolean whileIsTrue = true;
+        while (whileIsTrue == true) {
+
+            try {
+
+                if (alt == 1) {
+
+                } else if (alt == 2) {
+
+                }
+
+
+            } catch (Exception e) {
+                System.out.println("You can only write numbers");
+            }
+        }
+    }
+
 }
